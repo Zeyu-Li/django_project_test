72b9028da2f3552dbd9bca40b99d79f7ad30766e
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-04-11 11:30:11 -0600

    templete set

--- login/userpass/views.py
+++ login/userpass/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+
+    return render(request, 'userpass/home.html')
+
+def login(request):
+
+    return render(request, 'userpass/login.html')
