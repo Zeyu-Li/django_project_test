10a09853fa91d6ef4dce9feeb8d080f2ccdc322a
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-04-17 11:29:06 -0600

    fixed password input

--- login/userpass/forms.py
+++ login/userpass/forms.py
@@ -4,4 +4,4 @@ class UserpassForm(forms.Form):
 
     username = forms.CharField(label='Username', max_length=100) 
 
-    password = forms.CharField(label='Password', max_length=100) 
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput) 
