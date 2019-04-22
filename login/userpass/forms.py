57a2db6e671e82ccf0491a99c762d73872419086
Andrew <zeyu.li23@gmail.com>
2019-04-22 17:09:28 -0600

    database inited

--- login/userpass/forms.py
+++ login/userpass/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from .models import User
 
-class UserpassForm(forms.Form):
+# class UserpassForm(forms.Form):
 
-    username = forms.CharField(label='Username', max_length=100) 
+#     username = forms.CharField(label='Username', max_length=100) 
 
-    password = forms.CharField(label='Password', max_length=100) 
+#     password = forms.CharField(label='Password', max_length=32, widget=forms.PasswordInput) 
+
+class UserpassForm(forms.ModelForm):
+    class Meta:
+        model = Pass
+        fields = ['password']
--- login/userpass/models.py
+++ login/userpass/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+
+        return self.title
+
+class Pass(models.Model):
+
+    password = models.CharField(max_length=32)
