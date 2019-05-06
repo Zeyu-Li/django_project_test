5caaf26529e24f7ecc2b88f4acff672590fb3d49
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-05-06 13:07:22 -0600

    Fixed input

--- login/userpass/forms.py
+++ login/userpass/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import User
 
-# class UserpassForm(forms.Form):
+class UserpassForm(forms.Form):
 
-#     username = forms.CharField(label='Username', max_length=100) 
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', max_length=32, widget=forms.PasswordInput)
 
-password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput) 
+# password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
