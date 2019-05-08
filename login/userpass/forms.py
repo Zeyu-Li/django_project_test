be448e260fd6c215bf141a896e20624802d36c2a
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-05-08 11:51:57 -0600

    added min length for password

--- login/userpass/forms.py
+++ login/userpass/forms.py
@@ -4,6 +4,6 @@ from .models import User
 class UserpassForm(forms.Form):
 
     username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', max_length=32, widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', max_length=32, min_length=8, widget=forms.PasswordInput)
 
 # password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
